9491f146d6a91cdef2e63108161aff3a596e0b01
Stefan Wahren <stefan.wahren@i2se.com>
2015-04-04 16:19:32 +0200

    Reorder header includes

--- drivers/input/misc/mxs_pswitch.c
+++ drivers/input/misc/mxs_pswitch.c
@@ -12,18 +12,18 @@
  * http://www.gnu.org/copyleft/gpl.html
  */
 
-#include <linux/slab.h>
+#include <linux/init.h>
+#include <linux/input.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/ioport.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/interrupt.h>
-#include <linux/input.h>
-#include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_device.h>
-#include <linux/ioport.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 
 #define KEY_PRESSED		1
 #define KEY_RELEASED		0
